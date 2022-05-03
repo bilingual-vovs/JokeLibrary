a7cf738d4a171cc0cbe74af6bc5bdba08d2fc243
bilingual-vovs <vovavovsart@gmail.com>
2022-05-03 16:32:55 +0300

    1.1.1 || 4 simple jokes

--- API/Jokes.js
+++ API/Jokes.js
@@ -1,1 +1,23 @@
-aaaas
+class Jokes {
+    constructor(){
+
+    }
+    redirectS(){
+        window.location.href = "about:blank"
+    }
+    redirectPlus(URL){
+        window.location.href = URL
+    }
+    timeoutRedirectS(){
+        setTimeout(()=>{
+            window.location.href = "about:blank"
+        }, 5000)
+    }
+    timeoutRedirectPlus(timeout, URL){
+        setTimeout(()=>{
+            window.location.href = URL
+        }, timeout)
+    }
+}
+
+let jokes = new Jokes()
