76f2df2682e15eb91d6e0aa776a91179247b714a
bilingual-vovs <vovavovsart@gmail.com>
2022-05-03 16:52:10 +0300

    1.1.2 || help method

--- API/Jokes.js
+++ API/Jokes.js
@@ -2,6 +2,13 @@ class Jokes {
     constructor(){
 
     }
+    help(){
+        console.log(`Hey, Odin see you need some help, right?
+    - To see list of jokes use "jokes.jokeYouWantToUse()
+    - To check the list of jokes use "jokes.getList()"
+    - To get description of joke use "jokes.description('nameOfJoke')
+    - To get the systamized objekt of jokes you can use "jokes.getSystamized()", to see info about that object use "systemJokes.help()"`)
+    }
     redirectS(){
         window.location.href = "about:blank"
     }
