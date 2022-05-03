de97d5393bf35dc376da39c6acb7705fc40f0f46
bilingual-vovs <vovavovsart@gmail.com>
2022-05-03 16:22:20 +0300

    1.1.0 || apikey checking

--- API/server.js
+++ API/server.js
@@ -1,8 +1,23 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
+const fs = require("fs")
+
+const apikeys = fs.readFileSync(__dirname + "/apiKeys.json", '', (error, data)=>{if(error) throw error;})
+const apiKeysJson = JSON.parse(apikeys)
+let apiKeysSet = new Set(apiKeysJson)
 
 let appPort = process.argv[2]
 
-app.use("/library", express.static(__dirname + "/Jokes.js"))
+app.get("/library", (req, res)=> {
+    if (apiKeysSet.has(req.query.apikey)){
+        res.sendFile(__dirname + "/Jokes.js")
+        console.log("Aa")
+    }
+    else{
+        res.send("Unaloved")
+    }
+    
+})
 
 app.listen(appPort)
